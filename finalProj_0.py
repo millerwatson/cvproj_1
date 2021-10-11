d4e0e1009cc6d008cf76f3169afa2b688df80e87
millerwatson <55409077+millerwatson@users.noreply.github.com>
2021-10-10 23:40:50 -0700

    Added edge detection for all edges in the image

---
+++ finalProj_0.py
@@ -0,0 +1,47 @@
+import argparse
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+"""This program takes in a street sign from the user and returns what type of
+   sign it is. It works by getting the amount of sides on the sign's contour
+   and comparing those with a list of sign types. It also provides a color
+   histogram for the user to analyze the color of the sign.
+   Author: Miller Watson
+   Date: 10/1/21"""
+
+
+
+def main():
+
+
+    # loads and displays image
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--image", required = True, help = "Path to the image")
+    args = vars(ap.parse_args())
+
+    image = cv2.imread(args["image"])
+    cv2.imshow("Original", image)
+
+    # prepares the image for edge detection
+    blurImg = cv2.GaussianBlur(image, (7, 7), 0)
+    grayBlur = cv2.cvtColor(blurImg, cv2.COLOR_BGR2GRAY)
+
+    #displays prepared image
+    cv2.imshow("Grayed and Blurred", grayBlur)
+    cv2.waitKey(0)
+
+    # applies the Canny algorithm for edge detection
+    edges = cv2.Canny(grayBlur, 0, 155)
+    (contours, nothing) = cv2.findContours(edges.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Draws all of the contours over the original image
+    contourImg = image.copy()
+    cv2.drawContours(contourImg, contours, -1, (0,0,255), 3)
+    cv2.imshow("All contours", contourImg)
+    cv2.waitKey(0)
+
+
+
+main()
